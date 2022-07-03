cad9f230d5456a44728b41b0f53008bb45d66092
cggonzal <gonzalezcgg1@gmail.com>
2022-07-03 13:59:17 -0700

    turns out the browser gets the <video> tag in chunks by default. So no need for fancy dash protocols if you are doing stuff over LAN I guess

--- main.go
+++ main.go
@@ -7,11 +7,14 @@ import (
 
 func serveContent(fs http.Handler) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+
+		log.Print("request path: ", r.URL.Path)
+		log.Print("request headers: ", r.Header)
 		// add mimetype="application/dash+xml" for .mpd files
 		if len(r.URL.Path) > 4 && r.URL.Path[len(r.URL.Path)-4:] == ".mpd" {
 			w.Header().Add("mimetype", "application/dash+xml")
 		}
-		log.Print("url path is:", r.URL.Path)
+
 		fs.ServeHTTP(w, r)
 	}
 }
